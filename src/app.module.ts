261297bbe1e1b21813259af84d6809b8046356b3
kishan.ethics <kishan.dudhatra@ethicsinfoetech.in>
2026-04-21 12:27:57 +0530

    fix: added @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,32 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { LocationModule } from './location/location.module';
+
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }), // 🔥 ADD THIS
-
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT as string),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      autoLoadEntities: true,
-      synchronize: true,
+    // ✅ Global config
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('POSTGRES_HOST'),
+        port: parseInt(config.get<string>('POSTGRES_PORT') || '5432'),
+        username: config.get<string>('POSTGRES_USER'),
+        password: config.get<string>('POSTGRES_PASSWORD'),
+        database: config.get<string>('POSTGRES_DB'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+
     LocationModule,
   ],
   controllers: [AppController],
