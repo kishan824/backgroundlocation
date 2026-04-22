71ed36c714f783c8796a7bd74136f2fee479a1a5
kishan.ethics <kishan.dudhatra@ethicsinfoetech.in>
2026-04-22 11:36:36 +0530

    [KD] remove  unnesscry  import file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import open from 'open';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
